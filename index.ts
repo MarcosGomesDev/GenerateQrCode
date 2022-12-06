f957632ebe5ab01c8c65919c05321c24dd288ed3
Marcos Gomes <marcosgomesdev@gmail.com>
2022-12-06 20:39:11 -0300

    implementação da lib qrcode e lógica

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('OLA MUNDO')
+// @ts-nocheck
+
+import qr from 'qrcode';
+
+const generateQR = async text => {
+    try {
+        await qr.toFile('qr.png',text)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+generateQR('https://candy-cake-api.vercel.app/')
