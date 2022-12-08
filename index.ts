57af9ac119a5c4b4815e9c5b9f8562ee51cd2701
Marcos Gomes <marcosgomesdev@gmail.com>
2022-12-08 18:14:21 -0300

    upload qrcode to cloudinary

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
 // @ts-nocheck
-
 import qr from 'qrcode';
+import fs from 'node:fs'
+import cloud from './cloud'
+
 
 const generateQR = async text => {
     try {
-        await qr.toFile('qr.png',text)
+        const image = await qr.toDataURL(text)
+
+        const result = await cloud.uploader.upload(image, {
+            public_id: Math.floor(10000 + Math.random() * 90000) + 'qr',
+            width: 500,
+            height: 500,
+            crop: "fill"
+        })
+
+        console.log(result)
     } catch (error) {
         console.log(error)
     }
